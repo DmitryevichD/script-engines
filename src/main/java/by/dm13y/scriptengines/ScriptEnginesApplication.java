6b72f6273be4d1ca83fee9e4da9ba0af0905d4c3
DmitrievichD <d.dmitrievich@itransition.com>
2021-05-30 16:32:12 +0300

    init

---
+++ src/main/java/by/dm13y/scriptengines/ScriptEnginesApplication.java
@@ -0,0 +1,13 @@
+package by.dm13y.scriptengines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScriptEnginesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScriptEnginesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/dm13y/scriptengines/ScriptEnginesApplicationTests.java
@@ -0,0 +1,13 @@
+package by.dm13y.scriptengines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScriptEnginesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
